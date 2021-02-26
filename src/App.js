2c4a75ee9afe7b213e4bd03d9b4d0f9ab716d8fb
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2021-02-26 00:06:36 -0500

    added functionality to calculator and displaying of results

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
+// NPM IMPORTS
+import { useState } from "react";
+import { useForm } from 'react-hook-form';
+
+// COMPONENTS
 import TextField from './components/text-field/text-field';
+
+// CSS
 import './App.css';
+
 
 function App() {
 
-    function onSubmit(event) {
-        event.preventDefault();
+    // STATE
+    const [totalCalories, setTotalCalories] = useState(0);
+    const [fatCalories, setFatCalories] = useState(0);
+    const [carbCalories, setCarbCalories] = useState(0);
+    const [proteinCalories, setProteinCalories] = useState(0);
+
+    // FORM LOGIC
+    const { register, handleSubmit } = useForm();
+    const onSubmit = async (macros) => {
+        console.log(macros);
+        await setFatCalories(+macros.fat * 9);
+        await setCarbCalories(+macros.carbs * 4);
+        await setProteinCalories(+macros.protein * 4);
+        setTotalCalories(fatCalories + carbCalories + proteinCalories);
     }
 
-  return (
-      <div className="App">
-          <h1>Calorie Calculator</h1>
-          <form>
-              <TextField
-                name="Fat"
-              />
-              <TextField
-                  name="Carbs"
-              />
-              <TextField
-                  name="Protein"
-              />
-              <button type="submit"
-                onClick={onSubmit}
-              >Calculate</button>
-          </form>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Calorie Calculator</h1>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <TextField
+                    name="fat"
+                    reference={register}
+                />
+                <TextField
+                    name="carbs"
+                    reference={register}
+                />
+                <TextField
+                    name="protein"
+                    reference={register}
+                />
+                <button type="submit"
+                >Calculate</button>
+            </form>
+            <section>
+                <h2>Total Calories: {totalCalories}</h2>
+                <h3>Calories from...</h3>
+                <ul>
+                    <li>Fat: {fatCalories}</li>
+                    <li>Carbs: {carbCalories}</li>
+                    <li>Protein: {proteinCalories}</li>
+                </ul>
+            </section>
+        </div>
+    );
 }
 
 export default App;
--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function TextField({ name }) {
+export default function TextField({ name, reference }) {
     return (
         <div>
             <label htmlFor={name}>{name}: </label>
-            <input type="number" name={name} id={name} size="10"/>
+            <input type="number" name={name} ref={reference} id={name} size="10"/>
         </div>
     )
 }
